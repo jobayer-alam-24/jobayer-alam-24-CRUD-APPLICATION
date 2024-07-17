ab835f59c6d0cdf65d2183e110b2a14374b62464
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-07-17 16:01:05 +0600

    PHP-OOP

--- server/serverconnection.php
+++ server/serverconnection.php
@@ -1,84 +1,100 @@
 <?php
 
-    class DataBaseConnection{
-        public $hostName;
-        public $userName;
-        public $password;
-        public $db_name;
-        public $connect;
-        public function __construct($hostName, $userName, $password, $db_name){
-            $this->hostName = $hostName;
-            $this->userName = $userName;
-            $this->password = $password;
-            $this->db_name = $db_name;
+interface DatabaseOperations
+{
+    public function insert($name, $phone, $email, $pass, $conPassword);
+    public function select();
+    public function edit($id);
+    public function editSubmit($name, $phoneNumber, $email, $password, $id);
+    public function delete($id);
+}
 
-            $this->connect = mysqli_connect($hostName, $userName, $password, $db_name);
+trait DatabaseConnectionTrait {
+    public $hostName;
+    public $userName;
+    public $password;
+    public $db_name;
+    public $connect;
 
-            if (!$this->connect) {
-                die("Connection failed: " . mysqli_connect_error());
-            } else {
-                // echo "Connected successfully";
-            }
+    public function connectDatabase() {
+        $this->connect = mysqli_connect($this->hostName, $this->userName, $this->password, $this->db_name);
 
+        if (!$this->connect) {
+            die("Connection failed: " . mysqli_connect_error());
         }
-       public function insert($name, $phone, $email, $pass, $conPassword){
+    }
+}
+class DataBaseConnection implements DatabaseOperations {
+    use DatabaseConnectionTrait;
+
+    public function __construct($hostName, $userName, $password, $db_name){
+        $this->hostName = $hostName;
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->db_name = $db_name;
+
+        $this->connectDatabase();
+    }
+
+    public function insert($name, $phone, $email, $pass, $conPassword){
         $query = "INSERT INTO user_signup_info(name, phone_number, email, password, confirm_password) VALUES ('$name','$phone','$email','$pass','$conPassword')";
 
-        if(mysqli_query($this->connect, $query) == True){
+        if(mysqli_query($this->connect, $query)){
             // echo "Inserted";
         }else{
-            echo "Error: " + mysqli_error($this->connect);
+            echo "Error: " . mysqli_error($this->connect);
         }
-       }
-       public function select(){
+    }
+
+    public function select(){
         $query = "SELECT * FROM user_signup_info";
         $conn = mysqli_query($this->connect, $query);
 
-        if($conn == True){
+        if($conn){
             // echo "Selected";
         }else{
-            echo "Error: " + mysqli_error($this->connect);
+            echo "Error: " . mysqli_error($this->connect);
         }
         return $conn;
-       
-       }
-       public function edit($id){
-        $query = 'SELECT * FROM user_signup_info WHERE id = ' . $id;
+    }
 
+    public function edit($id){
+        $query = 'SELECT * FROM user_signup_info WHERE id = ' . $id;
         $conn = mysqli_query($this->connect, $query);
 
-        if($conn == True){
+        if($conn){
             // echo "Edited";
         }else{
-            echo "Error: " + mysqli_error($this->connect);
+            echo "Error: " . mysqli_error($this->connect);
         }
         return $conn;
-       }
-       public function EditSubmit($name, $phoneNumber, $email, $password, $id){
-        $query = "UPDATE user_signup_info SET name = '$name', phone_number = '$phoneNumber', email = '$email', password = '$password' WHERE id = $id";
+    }
 
+    public function editSubmit($name, $phoneNumber, $email, $password, $id){
+        $query = "UPDATE user_signup_info SET name = '$name', phone_number = '$phoneNumber', email = '$email', password = '$password' WHERE id = $id";
         $conn = mysqli_query($this->connect, $query);
 
-        if($conn == True){
+        if($conn){
             echo "Edit Submitted";
         }else{
-            echo "Error: " + mysqli_error($this->connect);
+            echo "Error: " . mysqli_error($this->connect);
         }
         return $conn;
-       }
-       public function delete($id){
-        $query = 'DELETE FROM user_signup_info WHERE id = ' . $id;
+    }
 
+    public function delete($id){
+        $query = 'DELETE FROM user_signup_info WHERE id = ' . $id;
         $conn = mysqli_query($this->connect, $query);
 
-        if($conn == True){
+        if($conn){
             echo "Deleted";
         }else{
-            echo "Error: " + mysqli_error($this->connect);
+            echo "Error: " . mysqli_error($this->connect);
         }
         return $conn;
-       }
     }
-    $conn = new DataBaseConnection("localhost", 'root', "", "schedule_manager");
-    
-?>
+}
+
+$conn = new DataBaseConnection("localhost", 'root', "", "schedule_manager");
+
+
